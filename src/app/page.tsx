4891cffd14108bdfe5fd139c619fb2ade0c2b1af
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-04-07 19:55:27 -0300

    feat: seed de posts  para inicio de projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-sm">Texto no meu h1</h1>
+      <h1 className='text-sm'>Texto no meu h1</h1>
     </div>
   )
 }
