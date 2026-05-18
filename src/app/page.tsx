c9f859606b9796585693315f0eb235ce231b6ec2
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-18 18:14:21 -0300

    feat: inicio de feature posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,18 @@ export default async function HomePage() {
 
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
         <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image className="group-hover:scale-105 transition" src="/images/bryen_0.png" width={1200} height={720} alt="Titulo do Post"/>
+          <Image className="group-hover:scale-105 transition w-full h-full object-cover object-center" priority src="/images/bryen_0.png" width={1200} height={720} alt="Titulo do Post"/>
         </Link>
-        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ipsum a sit eaque minus, architecto excepturi ex labore nostrum totam libero, error asperiores ipsam, quo dolor itaque recusandae. Totam, ad!
-        Molestias corporis voluptas error qui, voluptates animi, repellendus provident itaque ipsum impedit veniam inventore omnis alias laborum vel iusto atque nulla cum non dicta autem ipsa voluptatem. Eius, nobis officia.</div>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 block text-sm/tight" dateTime="2025-04-20">20/04/2025 10:00</time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">Lorem ipson dolor sit</Link>
+          </h1>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ipsum a sit eaque minus, architecto excepturi ex labore nostrum totam libero, error asperiores ipsam, quo dolor itaque recusandae. Totam, ad!
+        Molestias corporis voluptas error qui, voluptates animi, repellendus provident itaque ipsum impedit veniam inventore omnis alias laborum vel iusto atque nulla cum non dicta autem ipsa voluptatem. Eius, nobis officia.</p>
+          </div>
       </section>
 
       <Suspense fallback={<SpinLoader />}>
