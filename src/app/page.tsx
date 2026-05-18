08a9f5f275952d08c3f3409fd71a65d3edd9d896
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-18 17:54:13 -0300

    feat: css para header e section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import PostList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
-
   return (
-    <div>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é a Header</h1>
-      </header>
+    <Container>
+      <Header />
 
-      <Suspense fallback={<SpinLoader/>}>
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image className="group-hover:scale-105 transition" src="/images/bryen_0.png" width={1200} height={720} alt="Titulo do Post"/>
+        </Link>
+        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ipsum a sit eaque minus, architecto excepturi ex labore nostrum totam libero, error asperiores ipsam, quo dolor itaque recusandae. Totam, ad!
+        Molestias corporis voluptas error qui, voluptates animi, repellendus provident itaque ipsum impedit veniam inventore omnis alias laborum vel iusto atque nulla cum non dicta autem ipsa voluptatem. Eius, nobis officia.</div>
+      </section>
+
+      <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
       <footer>
         <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
       </footer>
-    </div>
-  )
+    </Container>
+  );
 }
