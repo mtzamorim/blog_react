9ab0d084d99b21477a9f0ecf6b4bdb7149f3ad26
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-19 11:16:04 -0300

    refactor: movido trechos para componente de postFeatured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
-import { PostCoverImage } from "@/components/PostCoverImage";
-import { PostHeading } from "@/components/PostHeading";
+import { PostFeatured } from "@/components/PostFeatured";
 import PostList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -11,47 +10,18 @@ export default async function HomePage() {
     <Container>
       <Header />
 
-      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <PostCoverImage
-          linkProps={{ href: "/post/asdasd"}}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: "/images/bryen_9.png",
-            alt: "Alt da imagem",
-            priority: true
-          }}
-        />
-        <div className="flex flex-col gap-4 sm:justify-center">
-          <time
-            className="text-slate-600 block text-sm/tight"
-            dateTime="2025-04-20"
-          >
-            20/04/2025 10:00
-          </time>
-
-          <PostHeading url="#" as="h1">
-            Esse é um titulo
-          </PostHeading>
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
-            ipsum a sit eaque minus, architecto excepturi ex labore nostrum
-            totam libero, error asperiores ipsam, quo dolor itaque recusandae.
-            Totam, ad! Molestias corporis voluptas error qui, voluptates animi,
-            repellendus provident itaque ipsum impedit veniam inventore omnis
-            alias laborum vel iusto atque nulla cum non dicta autem ipsa
-            voluptatem. Eius, nobis officia.
-          </p>
-        </div>
-      </section>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
+
       <footer>
         <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
       </footer>
+      
     </Container>
   );
 }
