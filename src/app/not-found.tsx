3fed238e84c60999e4cce32b89a1fb67e70e1428
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-20 18:25:10 -0300

    feat: tratamento para titulo e descrição da pagina

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 export default function NotFoundPage() {
   return (
+    <>
+    <title>Página não encontrada!</title>
     <div className="min-h-80 bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
       <div>
         <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
@@ -9,5 +11,6 @@ export default function NotFoundPage() {
         </p>
       </div>
     </div>
+    </>
   );
 }
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,20 +1,33 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PostSlugPageProps = {
-    params: Promise<{slug: string}>;
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetaData({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findPostBySlugCached(slug);
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
 }
 
-export default async function PostSlugPage({params} : PostSlugPageProps){
-    const { slug } = await params;
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
 
-   const post = await findPostBySlugCached(slug).catch(() => undefined)
+  const post = await findPostBySlugCached(slug).catch(() => undefined);
 
-    if(!post)notFound();
+  if (!post) notFound();
 
-    return(
-       <div>
-        <p>{post.content}</p>
-       </div>
-    )
+  return (
+    <div>
+      <p>{post.content}</p>
+    </div>
+  );
 }
