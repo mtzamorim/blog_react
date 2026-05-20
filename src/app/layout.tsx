a821c3a9396311918e9bc6516e6d72e46f6fa452
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-19 21:27:52 -0300

    feat: titulos estáticos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,32 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "The blog",
+  title: {
+    default: "The blog - Este é um blog com Next.js",
+    template: "%s | The Blog"
+  },
   description: "Esse é um blog com Next.js",
 };
 
 type RootLayoutProps = {
   children: React.ReactNode;
-}
+};
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <html
-      lang="pt-BR">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body>
+        <Container>
+          <Header />
+          {children}
+          <footer>
+            <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
 import { PostFeatured } from "@/components/PostFeatured";
 import PostList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
@@ -7,9 +5,7 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -17,11 +13,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
-
-      <footer>
-        <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
-      </footer>
-      
-    </Container>
+    </>
   );
 }
