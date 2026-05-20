ff1461de009914af5d9e177315454df7f9f76fc9
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2026-05-20 16:05:44 -0300

    refactor: alterado propriedade css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
   return (
-    <div className="min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
+    <div className="min-h-80 bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
       <div>
         <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
         <p>
